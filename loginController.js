badb04353ad89af1f8db3c00d8013e98d863ce7e
Sujay Talukdar <sujaytalukder03@gmail.com>
2018-12-17 09:47:34 +0530

    Update loginController.js

--- loginController.js
+++ loginController.js
@@ -3,8 +3,8 @@
 
     angular.module("app").controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
+    LoginController.$inject = ['$location', 'AuthenticationService'];
+    function LoginController($location, AuthenticationService) {
         var vm = this;
 
         vm.login = login;
